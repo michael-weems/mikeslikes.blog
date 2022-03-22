dcdacfe23c4f847037b7a75380492ea318a8038f
Michael Weems <mweems5@gmail.com>
2022-03-21 19:45:12 -0500

    Add floating action button to open search

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import './style.css';
-import { isNotDefined } from './utils';
+import { isNotDefined, whenElementReady } from './utils';
 import './components/command-palette';
 import './components/article';
 import './components/search';
+import { openCommandPalette } from './commands/open-command-palette';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 if (isNotDefined(app)) throw new Error('Could not find the app HTML Element!');
 
 app.innerHTML = `
+  <img src="https://i.redd.it/itafxefzhd0z.jpg"/>
   <div id="command-palette"></div>
   <div id="article"></div>
+  
+  <div class="fab-wrapper">
+    <label class="fab">
+      <span class="material-icons md-48 md-light">search</span>
+    </label>
+  </div>
+</div>
 `;
+
+whenElementReady('.fab').then((fab) => {
+  fab.addEventListener('click', () => {
+    openCommandPalette({mode: 'search'})
+  })
+})
